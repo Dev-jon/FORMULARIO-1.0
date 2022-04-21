705394f84ef0ed3d826a209062771523fe30b1c6
Jonatan Monteiro <monteirojonatan9@gmail.com>
2022-04-21 19:16:18 -0300

    second commit

---
+++ SCRIPTS/script.js
@@ -0,0 +1,32 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]')
+    const inputPassword = document.querySelector('input[type="password"]')
+    const submitButton = document.querySelector('.login__submit')
+
+    if (submitButton) {
+        submitButton.addEventListener('click', event => {
+            event.preventDefault()
+
+            fetch('https://regres.in/api/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: inputEmail.value,
+                        password: inputPassword.value
+                    })
+                })
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    console.log(data)
+                })
+        })
+    }
+
+    console.log(inputEmail, inputPassword, submitButton)
+}
+
+window.onload = init
